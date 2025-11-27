ff97fbd8ca78abac9298fce697fd3451dba7fbbd
ccyj003 <ccyj003@gmail.com>
2025-11-27 15:53:05 +0900

    Update test_ballcount_debug.cpp

--- test_ballcount_debug.cpp
+++ test_ballcount_debug.cpp
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>      // printf, scanf를 사용하기 위해서 헤더 추가
+#include <stdlib.h>     // rand, srand를 사용하기 위해서 헤더 추가
+#include <time.h>       // time 함수를 사용하기 위해서 헤더 추가
 
-int main(void)
-{
-    int ball[3];
-    srand(time(NULL));
+int main(void)      // 프로그램을 실행하는 main 함수
+{                   // 메인함수의 시작
+    int ball[3];            // 3개의 난수 저장 배열
+    srand(time(NULL));      // 난수 초기화
 
     do {
-        ball[0] = rand() % 10;
-        ball[1] = rand() % 10;
-        ball[2] = rand() % 10;
-    } while (ball[0] == ball[1] || ball[0] == ball[2] || ball[1] == ball[2]);
+        ball[0] = rand() % 10;      // 0~9까지의 난수를 생성하여 ball[0]에 저장
+        ball[1] = rand() % 10;      // 0~9까지의 난수를 생성하여 ball[1]에 저장
+        ball[2] = rand() % 10;      // 0~9까지의 난수를 생성하여 ball[2]에 저장
+    } while (ball[0] == ball[1] || ball[0] == ball[2] || ball[1] == ball[2]);       // ball[0]과 ball[1]의 숫자가 같으면 다시 생성
 
     printf("baseball : %d %d %d\n", ball[0], ball[1], ball[2]);
 
@@ -31,17 +31,17 @@ int main(void)
     int strike_count = 0;
     int ball_count = 0 ;
     int out_count = 3 ;
-    if( ball[0] == input[0] )       // strike
+    if( ball[0] == input[0] )       // strike count
     {
         strike_count = strike_count + 1 ;
         out_count = out_count - 1 ;
     }
-    if( ball[0] == input[1] )     // ball
+    if( ball[0] == input[1] )     // ball count
     {
         ball_count = ball_count + 1 ;
         out_count = out_count - 1 ;
     }
-    if( ball[0] == input[2] )     // ball
+    if( ball[0] == input[2] )     // ball count
     {
         ball_count = ball_count + 1 ;
         out_count = out_count - 1 ;
